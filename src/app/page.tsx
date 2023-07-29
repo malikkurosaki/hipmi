ca66fde1dc6751f3749cb1d582f7bbd299241318
Bagasbanuna02 <bagasbanuna02@gmail.com>
2023-07-29 10:57:41 +0800

    feat(src/app/page): mengedit page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
+import LayoutLandingPage from "./dev/landing-page/layout";
+import PageLandingPage from "./dev/landing-page/page";
+import PageDev from "./dev/page";
 
 export default async function Page() {
 
   return (
     // <Page1 data={store.get("session")?.value} />
-    <div>
-      <center>
-        <h1>HIPMI PROJECT</h1>
-        <p>Bip Production@Team_wibu</p>
-        <a href="/dev">Project</a>
-      </center>
-    </div>
+    // <div>
+    //   <center>
+    //     <h1>HIPMI PROJECT</h1>
+    //     <p>Bip Production@Team_wibu</p>
+    //     <a href="/dev">Project</a>
+    //   </center>
+    // </div>
+    <LayoutLandingPage>
+      <PageLandingPage/>
+    </LayoutLandingPage>
   );
 }
