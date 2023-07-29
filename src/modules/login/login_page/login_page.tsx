0bd23dfc6c5fef3513cbaa7b26c56b6a35cceb9f
Bagasbanuna02 <bagasbanuna02@gmail.com>
2023-07-29 12:04:05 +0800

    feat(login_page): Deskripsi: - in progress login page

--- src/modules/login/login_page/login_page.tsx
+++ src/modules/login/login_page/login_page.tsx
@@ -1,12 +1,48 @@
-"use client"
+"use client";
 
-import { Button } from "@mantine/core"
-import { useRouter } from "next/navigation"
+import {
+  Box,
+  Button,
+  Flex,
+  Grid,
+  Group,
+  Paper,
+  Text,
+  TextInput,
+} from "@mantine/core";
+import { useShallowEffect } from "@mantine/hooks";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
-export function LoginPage(){
-    const router = useRouter()
-    return <>
-    <div>ini login</div>
-    <Button onClick={() =>router.push("/dev/registrasi") }>Register</Button>
+export function LoginPage() {
+  const router = useRouter();
+
+  //menunggu rendering client karena tampilan di mulai dirender pada server
+  const [waiting, setWaiting] = useState(false);
+  useShallowEffect(() => {
+    if (window) setWaiting(true);
+  }, []);
+
+  if (!waiting) return <></>;
+  return (
+    <>
+      <Grid justify={"center"} align={"center"} h={"100vh"} w={"100"}>
+        <Paper w={300} h={300} bg={"gray"}
+       
+        >
+          
+            <Group position="center">
+              <Text>LOGIN</Text>
+              <TextInput w={250} label="Email / Username" />
+              <TextInput w={250} label="Password" />
+            </Group>
+
+            <Group position="center">
+            </Group>
+          
+        </Paper>
+      </Grid>
+      {/* <Button onClick={() =>router.push("/dev/registrasi") }>Register</Button> */}
     </>
+  );
 }
