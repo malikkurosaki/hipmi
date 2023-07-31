11b879fb23851edd250b99edc66bdab7940c2b7e
Bagasbanuna02 <bagasbanuna02@gmail.com>
2023-07-31 23:21:32 +0800

    feat(color) Deskripsi: - color tone untuk menyelaraskan warna pada frontend

---
+++ src/fun/color_tone.ts
@@ -0,0 +1,9 @@
+export const WARNA = {
+  hijau_tua: "#297646",
+  hijau_muda: "#3da18d",
+  hijau_cerah: "#42c748",
+  kuning: "#fed630",
+  biru: "#3175b1",
+  merah: "#DE2E2D",
+  hitam: "#121517",
+};
