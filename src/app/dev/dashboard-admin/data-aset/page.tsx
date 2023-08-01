1173a06c149e60843f0112a4377e10db06892303
lukman <lukman@lukmans-MacBook-Air.local>
2023-08-01 14:03:42 +0800

    style(tampilan data aset): deskripsi: page data aset

---
+++ src/app/dev/dashboard-admin/data-aset/page.tsx
@@ -0,0 +1,12 @@
+import { DataAset } from '@/modules/dashboard_admin';
+import React from 'react';
+
+function PageDataAset() {
+  return (
+    <>
+    <DataAset/>
+    </>
+  );
+}
+
+export default PageDataAset;
