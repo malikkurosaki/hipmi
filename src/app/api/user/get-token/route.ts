ac7a4162afff634d376688035ab5e7700a5e5c92
Bagasbanuna02 <bagasbanuna02@gmail.com>
2023-10-03 09:53:32 +0800

    title: auto des: auto note:auto

--- src/app/api/user/get-token/route.ts
+++ src/app/api/user/get-token/route.ts
@@ -1,3 +1,4 @@
+import { getConfig } from '@/bin/config';
 import { unsealData } from 'iron-session';
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
@@ -5,7 +6,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const c = cookies().get("ssn");
   const data = JSON.parse(
-    await unsealData(c?.value as string, {password: process.env.PWD as string})
+    await unsealData(c?.value as string, {password: ((await getConfig()).server.password)})
   )
 
   return NextResponse.json(data);
