25064825864091d8800e53886a67cfd0f9bcab0d
Bagasbanuna02 <bagasbanuna02@gmail.com>
2023-10-05 13:54:27 +0800

    fix Desc: - Penambahan seeder bidang bisnis

--- src/app/api/seeder/route.ts
+++ src/app/api/seeder/route.ts
@@ -1,12 +1,29 @@
 import prisma from "@/app/lib/prisma";
-import userRole from "../../../bin/seeder/user_role.json";
 import { NextResponse } from "next/server";
+import userRole from "../../../bin/seeder/user_role.json";
+import bidangBisnis from "../../../bin/seeder/bidang_bisnis.json";
 
 export async function GET(req: Request) {
   const dev = new URL(req.url).searchParams.get("dev");
   if (dev === "DEV-HIPMI") {
     for (let i of userRole) {
       const data = await prisma.masterUserRole.upsert({
+        where: {
+          id: i.id.toString(),
+        },
+        update: {
+          id: i.id.toString(),
+          name: i.name,
+        },
+        create: {
+          id: i.id.toString(),
+          name: i.name,
+        },
+      });
+    }
+
+    for (let i of bidangBisnis) {
+      await prisma.masterBidangBisnis.upsert({
         where: {
           id: i.id.toString(),
         },
