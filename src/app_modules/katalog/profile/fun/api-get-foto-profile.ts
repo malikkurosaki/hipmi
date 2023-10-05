ecb9cd70c4dd044efe7e9b55594e0a014b1a553a
Bagasbanuna02 <bagasbanuna02@gmail.com>
2023-10-05 11:22:37 +0800

    title: auto des: auto note:auto

--- src/app_modules/katalog/profile/fun/api-get-foto-profile.ts
+++ src/app_modules/katalog/profile/fun/api-get-foto-profile.ts
@@ -3,14 +3,19 @@ import { myConsole } from "@/app/fun/my_console";
 import prisma from "@/app/lib/prisma";
 
 export async function getFotoProfile(id: any) {
-  const imgUrl = await prisma.images.findUnique({
-    where: {
-      id: id,
-    },
-    select: {
-      id: true,
-      url: true,
-    },
-  });
-  return imgUrl;
+  if(id === null){
+    myConsole("id null")
+  } else {
+    const imgUrl = await prisma.images.findUnique({
+      where: {
+        id: id,
+      },
+      select: {
+        id: true,
+        url: true,
+      },
+    });
+    return imgUrl;
+  }
+  
 }
