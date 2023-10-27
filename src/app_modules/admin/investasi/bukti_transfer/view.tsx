8709e1aa4603fb1fa7a5776e1ae5a032ae7cd70f
Bagasbanuna02 <bagasbanuna02@gmail.com>
2023-10-27 17:47:08 +0800

    title: auto des: auto note:auto

--- src/app_modules/admin/investasi/bukti_transfer/view.tsx
+++ src/app_modules/admin/investasi/bukti_transfer/view.tsx
@@ -13,44 +13,41 @@ import {
 import { IconChevronRight } from "@tabler/icons-react";
 
 export default function Admin_BuktiTransferInvestasi() {
-
-    const listUsername = [
-        {
-            id: 1
-        },
-        {
-            id: 2
-        },
-        {
-            id: 3
-        },
-        {
-            id: 4
-        },
-        {
-            id: 5
-        },
-        {
-            id: 6
-        },
-        
-    ]
+  const listUsername = [
+    {
+      id: 1,
+    },
+    {
+      id: 2,
+    },
+    {
+      id: 3,
+    },
+    {
+      id: 4,
+    },
+    {
+      id: 5,
+    },
+    {
+      id: 6,
+    },
+  ];
   return (
     <>
       {/* Box Username */}
       {listUsername.map((e) => (
-        <Paper  key={e.id} bg={"gray"} p={"md"} mb={"xs"}>
-        <Grid align="center">
-          <Grid.Col span={6}>
-            <Text>Username</Text>
-          </Grid.Col>
-          <Grid.Col span={4}>
-            <Badge variant="dot">Status Transfer</Badge>
-          </Grid.Col>
-        </Grid>
-      </Paper>
+        <Paper key={e.id} bg={"gray"} p={"md"} mb={"xs"}>
+          <Grid align="center">
+            <Grid.Col span={6}>
+              <Text>Username</Text>
+            </Grid.Col>
+            <Grid.Col span={4}>
+              <Badge variant="dot">Status Transfer</Badge>
+            </Grid.Col>
+          </Grid>
+        </Paper>
       ))}
-      
     </>
   );
 }
