ac432d1eb7b975d621889b0d2e13e366e806cf56
amel <amel@amel.local>
2024-05-23 16:36:48 +0800

    fix: dashboard admin
    Deskripsi:
    - page list user dan list admin
    - search list user dan list admin
    
    No Issues

--- src/app/dev/admin/developer/page.tsx
+++ src/app/dev/admin/developer/page.tsx
@@ -4,12 +4,12 @@ import adminDeveloper_funGetListAllUser from "@/app_modules/admin/developer/fun/
 import _ from "lodash";
 
 export default async function Page() {
-  const listUser = await adminDeveloper_funGetListAllUser();
-  const listAdmin = await adminDeveloper_funGetListAllAdmin();
+  const listUser = await adminDeveloper_funGetListAllUser({ page: 1 });
+  const listAdmin = await adminDeveloper_funGetListAllAdmin({ page: 1 });
 
   return (
     <>
-      <AdminDeveloper listUser={listUser as any} listAdmin={listAdmin as any} />
+      <AdminDeveloper listUser={listUser.data as any} pUser={listUser.nPage} listAdmin={listAdmin.data as any} pAdmin={listAdmin.nPage} />
     </>
   );
 }
