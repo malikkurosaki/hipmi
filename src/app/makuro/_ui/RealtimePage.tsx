5670feb08cf9902462e8e462def5dc6b74b381cc
Bagasbanuna02 <bagasbanuna02@gmail.com>
2024-09-23 10:18:31 +0800

    Test realtime by mas malik

---
+++ src/app/makuro/_ui/RealtimePage.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { Button, Group, Stack } from "@mantine/core";
+import { useShallowEffect } from "@mantine/hooks";
+import { useWibuRealtime } from "wibu-realtime";
+
+export function RealtimePage({ token }: { token: string }) {
+  const [data, setData] = useWibuRealtime({
+    project: "hipmi",
+    WIBU_REALTIME_TOKEN: token,
+  });
+
+  useShallowEffect(() => {
+    if (data) {
+      console.log(data);
+    }
+  }, [data]);
+
+  function onKirim() {
+    setData({
+      id: "123",
+      data: Math.random().toString(),
+    });
+  }
+  return (
+    <Stack p={"lg"}>
+      <Group>
+        <Stack>
+          {JSON.stringify(data)}
+          <Button onClick={onKirim}> Tekan Aja</Button>
+        </Stack>
+      </Group>
+    </Stack>
+  );
+}
