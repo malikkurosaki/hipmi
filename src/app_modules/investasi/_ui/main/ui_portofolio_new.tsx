c4b2a47c42763d7327d1616fa0bd036e517b7da2
amel <amel@amel.local>
2024-12-17 10:44:14 +0800

    upd: investasi portofolio
    Deskripsi:
    - ganti server action status portofolio menjadi data json
    
    No Issues

--- src/app_modules/investasi/_ui/main/ui_portofolio_new.tsx
+++ src/app_modules/investasi/_ui/main/ui_portofolio_new.tsx
@@ -1,14 +1,35 @@
 "use client";
 import { NEW_RouterInvestasi } from "@/app/lib/router_hipmi/router_investasi";
 import { AccentColor, MainColor } from "@/app_modules/_global/color";
-import { MODEL_NEW_DEFAULT_MASTER } from "@/app_modules/model_global/interface";
 import { Stack, Tabs } from "@mantine/core";
 import { useParams, useRouter } from "next/navigation";
 import { Investasi_ViewPortofolioNew } from "../../_view/main/portofolio/view_portofolio_new";
 
-export function Investasi_UiPortofolioNew({ listStatus }: { listStatus: MODEL_NEW_DEFAULT_MASTER[] }) {
+export function Investasi_UiPortofolioNew() {
    const param = useParams<{ id: string }>();
    const router = useRouter();
+   const status = [
+      {
+         id: "1",
+         name: "Publish",
+         color: "green"
+      },
+      {
+         id: "2",
+         name: "Review",
+         color: "orange"
+      },
+      {
+         id: "3",
+         name: "Draft",
+         color: "yellow"
+      },
+      {
+         id: "4",
+         name: "Reject",
+         color: "red"
+      }
+   ]
 
    return (
       <>
@@ -30,7 +51,7 @@ export function Investasi_UiPortofolioNew({ listStatus }: { listStatus: MODEL_NE
          >
             <Stack>
                <Tabs.List grow mb={"xs"}>
-                  {listStatus.map((e) => (
+                  {status.map((e) => (
                      <Tabs.Tab
                         w={"20%"}
                         key={e.id}
